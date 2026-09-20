95a3b28875763e3200e1479b432d0fa296eb04a1
Hyunjin Baek <hyunjin3399@gmail.com>
2026-09-21 00:36:29 +0900

    study interface basic

--- oop/interfaces/basic/InterfaceBasic.java
+++ oop/interfaces/basic/InterfaceBasic.java
@@ -1,14 +1,37 @@
 package oop.interfaces.basic;
 
-// 인터페이스와 구현 객체 사용
-// - 인터페이스도 참조 타입으로 사용할 수 있다.
-// - 인터페이스 타입의 변수에는 해당 인터페이스를 구현한 객체를 대입할 수 있다.
-// - 인터페이스 타입으로 메소드를 호출하면 실제 구현 객체의 메소드가 실행된다.
+// 인터페이스 선언
+// - interface 키워드로 선언한다.
+// - 인터페이스는 구현 클래스가 따라야 할 기능을 정의한다.
+// - 추상 메소드는 메소드의 형태만 작성하고 실행 내용은 작성하지 않는다.
+interface DeviceControl {
 
+    void turnOn();
+    void turnOff();
+}
+
+// 인터페이스 구현
+// - implements를 사용하여 인터페이스를 구현한다.
+// - 구현 클래스는 인터페이스의 추상 메소드를 모두 구현해야 한다.
+class Lamp implements DeviceControl {
+
+    @Override
+    public void turnOn() {
+        System.out.println("전등을 켭니다.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("전등을 끕니다.");
+    }
+}
+
+// 인터페이스와 구현 객체 사용
+// - 인터페이스 타입의 변수에는 구현 객체를 대입할 수 있다.
+// - 인터페이스 타입으로 선언된 메소드를 호출할 수 있다.
 public class InterfaceBasic {
 
     public static void main(String[] args) {
-        // 구현 클래스의 객체 생성
         Lamp lamp = new Lamp();
 
         lamp.turnOn();
